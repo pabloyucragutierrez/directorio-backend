ec76d2f8af539aa1c3851d050a9ed08603a025b3
pablo gutierrez <pabloyucragutierrez@gmail.com>
2026-04-09 21:39:20 -0500

    ultimo commit

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET') as string,
-    });
+    }); 
   }
 
   async validate(payload: any) {
